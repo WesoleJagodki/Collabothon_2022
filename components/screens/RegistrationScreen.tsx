6ccf9ea30dfc06bac95a79c8a876d8e565118473
Preacherek <sarna2000piotr@gmail.com>
2022-07-13 21:39:55 +0200

    RegistrationScreen is ready

--- components/screens/RegistrationScreen.tsx
+++ components/screens/RegistrationScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Text, View} from "react-native";
-import {Button, NativeBaseProvider, Image, VStack, Input} from "native-base"
+import {Button, NativeBaseProvider, Image, VStack, Input, HStack, Checkbox} from "native-base"
 
 import {global_style} from "../styles/GlobalStyle";
 import {registration_screen} from "../styles/RegistrationScreenStyle";
@@ -13,7 +13,14 @@ export const RegistrationScreen = function ({navigation} : any): JSX.Element {
     const [Gender, setGender] = React.useState("");
     const [PIN, setPIN] = React.useState("");
 
-    const handleChange = text => setValue(text);
+    const handleChangeUsername = text => setUsername(text);
+    const handleChangeEmail = text => setEmail(text);
+    const handleChangePassword = text => setPassword(text);
+    const handleChangeBirthDay = text => setBirthDay(text);
+    const handleChangeGender = text => setGender(text);
+    const handleChangePIN = text => setPIN(text);
+
+    const [checkboxValues, setCheckboxValues] = React.useState([]);
 
     return (
         <NativeBaseProvider>
@@ -22,17 +29,35 @@ export const RegistrationScreen = function ({navigation} : any): JSX.Element {
                     <Text style={registration_screen.text1}>Create a New Account</Text>
                     <Text style={registration_screen.text2}>Create an account and enter the</Text>
                     <Text style={registration_screen.text2}>world of wildlife</Text>
-                    <VStack style={registration_screen.vstack2} space={26} alignItems="center">
-                        <Input variant="rounded" borderRadius="10" shadow={4} style={registration_screen.input} placeholder="Username" value={Username} onChangeText={handleChange}/>
-                        <Input variant="rounded" borderRadius="10" shadow={4} style={registration_screen.input} placeholder="Email" value={Email} onChangeText={handleChange}/>
-                        <Input variant="rounded" borderRadius="10" shadow={4} style={registration_screen.input} placeholder="Password" value={Password} onChangeText={handleChange}/>
-                        <Input variant="rounded" borderRadius="10" shadow={4} style={registration_screen.input} placeholder="Birth Day" value={BirthDay} onChangeText={handleChange}/>
-                        <Input variant="rounded" borderRadius="10" shadow={4} style={registration_screen.input} placeholder="Gender" value={Gender} onChangeText={handleChange}/>
-                        <Input variant="rounded" borderRadius="10" shadow={4} style={registration_screen.input} placeholder="PIN" value={PIN} onChangeText={handleChange}/>
+                    <VStack style={registration_screen.vstack2} space={22} alignItems="center">
+                        <Input variant="rounded" borderRadius="10" shadow={4} h="7%" style={registration_screen.input} placeholder="Username" value={Username} onChangeText={handleChangeUsername}/>
+                        <Input variant="rounded" borderRadius="10" shadow={4} h="7%" style={registration_screen.input} placeholder="Email" value={Email} onChangeText={handleChangeEmail}/>
+                        <Input variant="rounded" borderRadius="10" shadow={4} h="7%" style={registration_screen.input} placeholder="Password" value={Password} onChangeText={handleChangePassword}/>
+                        <Input variant="rounded" borderRadius="10" shadow={4} h="7%" style={registration_screen.input} placeholder="Birth Day" value={BirthDay} onChangeText={handleChangeBirthDay}/>
+                        <Input variant="rounded" borderRadius="10" shadow={4} h="7%" style={registration_screen.input} placeholder="Gender" value={Gender} onChangeText={handleChangeGender}/>
+                        <Input variant="rounded" borderRadius="10" shadow={4} h="7%" style={registration_screen.input} placeholder="PIN" value={PIN} onChangeText={handleChangePIN}/>
+                        <Checkbox shadow={4} onChange={setCheckboxValues} value={checkboxValues} accessibilityLabel="I agree to the Terms of Service and Privacy Policy">
+                            <Text style={registration_screen.text3}>I agree to the Terms of Service and Privacy Policy</Text>
+                        </Checkbox>
+                        <Button style={registration_screen.button} colorScheme={"green"}>
+                            <Text style={registration_screen.text4}>Sign up</Text>
+                        </Button>
                     </VStack>
                 </VStack>
-
-
+                <HStack bottom={"4%"} space={1}>
+                    <Text style={registration_screen.text5}>Already have an account?</Text>
+                    <Text style={registration_screen.text6} onPress={() => navigation.navigate("Root")}>Log In</Text>
+                </HStack>
+                <Image
+                    style={registration_screen.picture1}
+                    source={require("../../images/registration-screen-1.png")}
+                    alt="Orientarium ZOO Łódź"
+                />
+                <Image
+                    style={registration_screen.picture2}
+                    source={require("../../images/registration-screen-2.png")}
+                    alt="Orientarium ZOO Łódź"
+                />
             </View>
         </NativeBaseProvider>
     );
--- components/styles/RegistrationScreenStyle.ts
+++ components/styles/RegistrationScreenStyle.ts
@@ -15,12 +15,49 @@ export const registration_screen = StyleSheet.create({
     },
     vstack2: {
         flex: 1,
-        width: "80%",
+        width: "85%",
         marginTop: 46,
     },
     input: {
+        flex: 1,
         backgroundColor: "#F6F6F6",
         color: "black",
         width: 300,
+    },
+    text3: {
+        fontSize: 13,
+        fontWeight: "bold",
+    },
+    button: {
+        width: "40%",
+        borderRadius: 10,
+    },
+    text4: {
+        fontWeight: "bold",
+        color: "white",
+        fontSize: 16,
+        right: "25%"
+    },
+    text5: {
+        fontSize: 13,
+    },
+    text6: {
+        fontSize: 13,
+        color: "green",
+        fontWeight: "bold"
+    },
+    picture1: {
+        position: "absolute",
+        width: 155,
+        height: 110,
+        bottom: "2%",
+        left: "0%"
+    },
+    picture2: {
+        position: "absolute",
+        width: 150,
+        height: 160,
+        right: "-5%",
+        bottom: "2%"
     }
 });
