9361037f92b0b21b97a95e32291f0d93ea7fc068
Andrew2a1 <adam.brz@onet.eu>
2022-07-13 17:42:34 +0200

    Add picture picker

---
+++ screens/PicturePickerScreen.tsx
@@ -0,0 +1,50 @@
+import { View } from '../components/Themed';
+import { useState } from 'react';
+import React from 'react';
+
+import { Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function PicturePickerScreen() {
+  const [image, setImage] = useState<null | string>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled && result.uri) {
+      setImage(result.uri);
+
+      let localUri = result.uri || "";
+      let filename = localUri.split('/').pop() || "";
+
+      let match = /\.(\w+)$/.exec(filename);
+      let type = match ? `image/${match[1]}` : `image`;
+      let formData = new FormData();
+
+      // @ts-ignore
+      formData.append('photo', { uri: localUri, name: filename, type });
+
+      console.log( { uri: localUri, name: filename, type });
+
+      await fetch("http://127.0.0.1:8000/image", {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      });
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
+  );
+}
