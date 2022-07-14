2d98a1631be13dc37f9145143e1edb195b290e5a
Andrew2a1 <adam.brz@onet.eu>
2022-07-14 14:39:47 +0200

    Fix footer scalling

--- components/footer/FooterStyle.ts
+++ components/footer/FooterStyle.ts
@@ -1,5 +1,6 @@
 import {Dimensions, StyleSheet} from "react-native";
 const screen_width = Dimensions.get('window').width;
+const screen_height = Dimensions.get('window').height;
 
 export const footer_style = StyleSheet.create({
     container: {
@@ -8,7 +9,7 @@ export const footer_style = StyleSheet.create({
         backgroundColor: "#5E9D75",
         bottom: 0,
         left: -0.5*screen_width,
-        height: "7%",
+        height: 0.07*screen_height,
         width: screen_width,
         justifyContent: "center",
         alignItems: "center"
