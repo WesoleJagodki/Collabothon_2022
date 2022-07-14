599e7568ea5f4f46b5a791f09afab66a2a76407b
Andrew2a1 <adam.brz@onet.eu>
2022-07-14 14:28:18 +0200

    Add highlight for current screen

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,16 +5,21 @@ import {NativeBaseProvider, Image, HStack} from "native-base"
 import {footer_style} from "./FooterStyle";
 import { FooterItem, FooterPage } from "./FooterItem";
 
-export const Footer = function ({navigation} : any): JSX.Element {
+interface Props {
+    navigation: any;
+    currentScreen: FooterPage;
+}
+
+export const Footer = function ({navigation, currentScreen} : Props): JSX.Element {
     return (
         <NativeBaseProvider>
             <View style={footer_style.container}>
                 <HStack space={10}>
-                    <FooterItem navigation={navigation} page={FooterPage.HOME} isSelected={false}/>
-                    <FooterItem navigation={navigation} page={FooterPage.QUESTS} isSelected={false}/>
-                    <FooterItem navigation={navigation} page={FooterPage.TICKETS} isSelected={false}/>
-                    <FooterItem navigation={navigation} page={FooterPage.MAP} isSelected={false}/>
-                    <FooterItem navigation={navigation} page={FooterPage.INFO} isSelected={false}/>
+                    <FooterItem navigation={navigation} page={FooterPage.HOME} isSelected={currentScreen === FooterPage.HOME}/>
+                    <FooterItem navigation={navigation} page={FooterPage.QUESTS} isSelected={currentScreen === FooterPage.QUESTS}/>
+                    <FooterItem navigation={navigation} page={FooterPage.TICKETS} isSelected={currentScreen === FooterPage.TICKETS}/>
+                    <FooterItem navigation={navigation} page={FooterPage.MAP} isSelected={currentScreen === FooterPage.MAP}/>
+                    <FooterItem navigation={navigation} page={FooterPage.INFO} isSelected={currentScreen === FooterPage.INFO}/>
                 </HStack>
             </View>
         </NativeBaseProvider>
--- components/screens/MainScreen.tsx
+++ components/screens/MainScreen.tsx
@@ -5,12 +5,13 @@ import {Button, NativeBaseProvider, Image} from "native-base"
 import {global_style} from "../styles/GlobalStyle";
 
 import {Footer} from "../footer/Footer";
+import { FooterPage } from "../footer/FooterItem";
 
 export const MainScreen = function ({navigation} : any): JSX.Element {
     return (
         <NativeBaseProvider>
             <View style={global_style.container}>
-                <Footer navigation={navigation} />
+                <Footer navigation={navigation} currentScreen={FooterPage.HOME} />
             </View>
         </NativeBaseProvider>
     );
