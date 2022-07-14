f9ecbc848cc55998ef36fea71ee493c86e6e82af
Preacherek <sarna2000piotr@gmail.com>
2022-07-14 19:23:54 +0200

    Header is ready

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,27 +7,40 @@ import { header_style } from "./HeaderStyle";
 interface Props {
     navigation: any;
     text: any;
+    goto: any;
+    picture: number;
+    account: number
 }
 
-export const Header = function ({ navigation, text} : Props): JSX.Element {
+const headerImages = {
+    header1: require("../../images/header-1.png"),
+    header2: require("../../images/header-2.png"),
+    header3: null,
+    account1: require("../../images/account-screen-1.png"),
+    account2: null,
+};
+
+export const Header = function ({ navigation, text, goto, picture, account} : Props): JSX.Element {
   return (
     <NativeBaseProvider>
       <View style={header_style.container}>
           <HStack>
-              <Image
-                  style={header_style.picture}
-                  source={require("../../images/header-2.png")}
-                  alt="Orientarium ZOO Łódź"
-              />
-              <Text style={header_style.text}>{text}</Text>
-              <Button onPress={() => navigation.navigate("LoginScreen")} style={header_style.button}>
+              <Button style={header_style.button1} onPress={() => navigation.navigate(goto)}>
                   <Image
-                      style={header_style.account}
-                      source={require("../../images/account-screen-1.png")}
+                      style={header_style.picture}
+                      source={headerImages[`header${picture}`]}
                       alt="Orientarium ZOO Łódź"
                   />
               </Button>
 
+              <Text style={header_style.text}>{text}</Text>
+              <Button style={header_style.button2} onPress={() => navigation.navigate("RegistrationScreen")}>
+                  <Image
+                      style={header_style.account}
+                      source={headerImages[`account${account}`]}
+                      alt="Orientarium ZOO Łódź"
+                  />
+              </Button>
           </HStack>
       </View>
     </NativeBaseProvider>
--- components/header/HeaderStyle.ts
+++ components/header/HeaderStyle.ts
@@ -6,7 +6,6 @@ export const header_style = StyleSheet.create({
   container: {
     position: "absolute",
     flex: 1,
-    backgroundColor: "blue",
     top: 0,
     left: -0.5 * screen_width,
     height: 0.1 * screen_height,
@@ -20,20 +19,23 @@ export const header_style = StyleSheet.create({
     fontSize: 24,
   },
   picture: {
-    position: "absolute",
     height: 30,
     width: 30,
-    top: 2,
-    right: 180
   },
   account: {
-    position: "absolute",
     height: 40,
     width: 40,
-    left: 70,
-    bottom: 0
   },
-  button: {
-    backgroundColor: "0"
+  button1: {
+    position: "absolute",
+    backgroundColor: 0,
+    right: 170,
+    top: "-25%"
+  },
+  button2: {
+    position: "absolute",
+    backgroundColor: 0,
+    left: 160,
+    top: "-60%"
   }
 });
