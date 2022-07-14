d12415d2686a91df46e89888790737f5cc03d305
Damian <damiann.szczepanskii@gmail.com>
2022-07-14 22:05:35 +0200

    remove .idea

--- components/screens/TicketsScreen.tsx
+++ components/screens/TicketsScreen.tsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { Button, NativeBaseProvider, Image } from "native-base";
+import { Button, NativeBaseProvider, Image, HStack, VStack } from "native-base";
 
 import { global_style } from "../styles/GlobalStyle";
 
 import { Footer } from "../footer/Footer";
 import { FooterPage } from "../footer/FooterItem";
+import {tickets_screen_style} from "../styles/TicketsScreenStyle";
 
 export const TicketsScreen = function ({ navigation }: any): JSX.Element {
   return (
     <NativeBaseProvider>
       <View style={global_style.container}>
+          <HStack style={tickets_screen_style.hstack1}>
+              <Image style={tickets_screen_style.picture1} source={require("../../images/shopping-cart 1.png")} />
+              <Text style={tickets_screen_style.text1}>Tickets</Text>
+          </HStack>
+          <VStack style={tickets_screen_style.vstack1}>
+              <HStack style={tickets_screen_style.hstack2}>
+                  <VStack>
+                      <Image style={tickets_screen_style.picture2} source={require("../../images/tickets-screen-1.png")} />
+                      <Text>wifnwiwowfno</Text>
+                  </VStack>
+                  <VStack>
+                      <Image style={tickets_screen_style.picture2} source={require("../../images/tickets-screen-2.png")} />
+                      <Text>wofnwoifwanfiwoanfiwainf</Text>
+                  </VStack>
+              </HStack>
+              <HStack style={tickets_screen_style.hstack2}>
+                  <VStack>
+                      <Image style={tickets_screen_style.picture2} source={require("../../images/tickets-screen-3.png")} />
+                      <Text>opfnwepifnif</Text>
+                  </VStack>
+                  <VStack>
+                      <Image style={tickets_screen_style.picture2} source={require("../../images/tickets-screen-4.png")} />
+                      <Text>wfnwoifwnf</Text>
+                  </VStack>
+              </HStack>
+          </VStack>
+
+
         <Footer navigation={navigation} currentScreen={FooterPage.TICKETS} />
       </View>
     </NativeBaseProvider>
--- components/styles/TicketsScreenStyle.ts
+++ components/styles/TicketsScreenStyle.ts
@@ -1,3 +1,34 @@
 import { StyleSheet } from "react-native";
 
-export const tickets_screen_style = StyleSheet.create({});
+export const tickets_screen_style = StyleSheet.create({
+    text1: {
+        fontWeight: "normal",
+        fontSize: 34,
+        alignContent: "center",
+    },
+    picture1: {
+        width: 30,
+        height: 30,
+    },
+    picture2: {
+        width: 150,
+        height: 150
+    },
+    hstack1: {
+        alignSelf: "flex-start",
+        backgroundColor: "red",
+        width: "100%",
+        padding: "10%"
+    },
+    hstack2: {
+        backgroundColor: "purple",
+        justifyContent: "space-between",
+        width: "100%",
+        padding: "10%"
+    },
+    vstack1: {
+        flex: 5,
+        backgroundColor: "green",
+        width: "100%"
+    }
+});
